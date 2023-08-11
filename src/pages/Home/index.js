ee09b09b2ee1bb04f1c5382cdbcdbe98849e53fe
Iago Becker <iago.mendonca20@gmail.com>
2023-08-11 19:06:37 -0300

    feat: add Register

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <Text style={styles.title}>Cadastro</Text>
+
+      <TextInput style={styles.input} placeholder="Nome" />
+      <TextInput style={styles.input} placeholder="E-mail" />
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        secureTextEntry={true}
+      />
+      <TextInput style={styles.input} placeholder="Aniversário" />
+      <TextInput style={styles.input} placeholder="CPF" />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Cadastre-se</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    paddingTop: 100,
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "black",
+    marginBottom: 20,
+  },
+  label: {
+    fontWeight: "bold",
+    fontSize: 18,
+    marginBottom: 5,
+    alignSelf: "flex-start",
+    color: "black",
+  },
+  input: {
+    width: "100%",
+    height: 90,
+    backgroundColor: "white",
+    borderRadius: 8,
+    marginBottom: 15,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    fontSize: 18,
+    color: "black",
+  },
+  button: {
+    backgroundColor: "#ec4760",
+    paddingVertical: 12,
+    paddingHorizontal: 40,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 18,
+    borderRadius: 15,
   },
 });
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native"
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 const Stack = createNativeStackNavigator();
 import Home from "./pages/Home/index";
+import Register from "./pages/Register";
 
 export default function Routes() {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Home" component={Home} />
-          </Stack.Navigator>
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Register" component={Register} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
