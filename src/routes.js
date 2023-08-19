aa9020e4ce8bc9d456108d0e34821b747a584b73
Arian Siqueira <arian.siqueira@gmail.com>
2023-08-19 19:24:08 -0300

    feat: Login Screen Implementation

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 const Stack = createNativeStackNavigator();
 import Home from "./pages/Home/index";
+import LoginScreen from "./pages/LoginScreen/index";
 
 export default function Routes() {
     return (
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
           <Stack.Screen name="Home" component={Home} />
           </Stack.Navigator>
     </NavigationContainer>
