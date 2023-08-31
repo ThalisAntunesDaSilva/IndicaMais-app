240219714ac12190a72dffe5a227a67b28e41b4d
Arian Siqueira <arian.siqueira@gmail.com>
2023-08-31 19:42:35 -0300

    feat: Impletation of IndicationsList

---
+++ src/pages/LoginScreen/styles.js
@@ -0,0 +1,95 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    form: {
+        width: '100%',
+    },
+    input: {
+        width: '100%',
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 10,
+        paddingHorizontal: 10,
+    },
+    signupContainer: {
+        marginTop: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    signupText: {
+        fontSize: 16,
+    },
+    signupLinkText: {
+        color: '#EC4760',
+        textDecorationLine: 'underline',
+        marginLeft: 5,
+    },
+    passwordInputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: 'gray',
+        paddingHorizontal: 20,
+    },
+    passwordInput: {
+        flex: 1,
+        height: 40,
+        paddingVertical: 5,
+        paddingRight: 5,
+    },
+    inputContainer: {
+        marginBottom: 10,
+    },
+    passwordInputWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: 'gray',
+        paddingHorizontal: 10,
+    },
+    passwordIcon: {
+        marginRight: 10,
+    },
+    inputLabel: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginBottom: 5,
+        textAlign: 'left',
+        color: '#A7A6A5',
+    },
+    loginButtonContainer: {
+        marginTop: 270,
+        alignItems: 'center',
+    },
+    loginButton: {
+        width: '60%',
+        paddingVertical: 15,
+        paddingHorizontal: 30,
+        backgroundColor: '#EC4760',
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputBackground: {
+        backgroundColor: '#EFF2F1',
+        borderWidth: 0,
+        borderRadius: 8,
+    },
+    loginButtonText: {
+        fontWeight: 'bold',
+        color: 'white',
+    }
+})
--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack"
 const Stack = createNativeStackNavigator();
 import Home from "./pages/Home/index";
 import LoginScreen from "./pages/LoginScreen/index";
+import Indications from "./pages/Indications/index";
 
 export default function Routes() {
     return (
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Indications" component={Indications} />
           <Stack.Screen name="LoginScreen" component={LoginScreen} />
           <Stack.Screen name="Home" component={Home} />
           </Stack.Navigator>
